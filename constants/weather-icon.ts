44fd18e05409481d0a7505d206539f782173cc5d
fajarizz <rafafajari230907@gmail.com>
2025-11-28 19:20:13 +0700

    weather icon map

---
+++ constants/weather-icon.ts
@@ -0,0 +1,37 @@
+export const weatherIcons: Record<number, string> = {
+    0: "weather-sunny",
+    1: "weather-sunny",
+    2: "weather-partly-cloudy",
+    3: "weather-cloudy",
+
+    45: "weather-fog",
+    48: "weather-fog",
+
+    51: "weather-rainy",
+    53: "weather-rainy",
+    55: "weather-rainy",
+
+    61: "weather-pouring",
+    63: "weather-pouring",
+    65: "weather-pouring",
+
+    66: "weather-pouring",
+    67: "weather-pouring",
+
+    71: "weather-snowy",
+    73: "weather-snowy",
+    75: "weather-snowy-heavy",
+
+    77: "weather-snowy",
+
+    80: "weather-pouring",
+    81: "weather-pouring",
+    82: "weather-pouring",
+
+    85: "weather-snowy-rainy",
+    86: "weather-snowy-rainy",
+
+    95: "weather-lightning",
+    96: "weather-lightning",
+    99: "weather-lightning",
+}
