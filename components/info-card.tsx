f5c42852672cab63d3c860180d3b1981fca4bbe6
fajarizz <rafafajari230907@gmail.com>
2025-11-28 19:25:43 +0700

    es lint haters

--- components/info-card.tsx
+++ components/info-card.tsx
@@ -1,7 +1,7 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Text, View } from "react-native";
 
-export default function InfoCard({ icon, title, value }: { icon: string, title: string, value?: string }) {
+export default function InfoCard({ icon, title, value }: { icon: keyof typeof MaterialCommunityIcons.glyphMap, title: string, value?: string }) {
     return (
         <View className="justify-between items-start bg-gray-800 min-h-44 flex-1 rounded-xl px-3 py-5">
             <View className="flex-row w-full justify-start items-center gap-2">
